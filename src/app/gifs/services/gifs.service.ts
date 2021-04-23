e1dea504ae15e98ad00af2fff2c62d1000f50deb
Cristian Saball <c.saball@externo.ibermatica.com>
2021-04-24 01:13:26 +0200

    No duplicar las palabras de la busqueda

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,9 +11,18 @@ export class GifsService {
     return [...this._historial];
   }
 
-  buscarGifs(query: string){
+  buscarGifs(query: string = ''){
 
-    this._historial.unshift(query); //unshift inserta al inicio
+    query = query.trim().toLowerCase(); //recibe en minuscula
+
+    //Este if comprueba que dentro del array no este ya el string que esta recibiendo
+    if(!this._historial.includes( query )){ //si query no esta dentro del array entonces..
+
+      this._historial.unshift(query); //unshift inserta al inicio
+      this._historial = this._historial.splice(0,10); //Tamaño de 10 maximo del array
+    }
+
+    
 
     console.log(this._historial);
   }
