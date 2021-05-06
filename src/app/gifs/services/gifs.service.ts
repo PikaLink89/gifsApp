ecc984292585841daeecab45337ace6083ed690a
Cristian Saball <c.saball@externo.ibermatica.com>
2021-05-06 22:03:02 +0200

    httpParams , cambiando la URL a algo mas profesional

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
@@ -8,6 +8,8 @@ import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 export class GifsService {
 
   private apiKey :string ='b3SH9Mch5rxj2Jtgzwnpn31OLw8YavrD';
+
+  private servicioUrl :string='https://api.giphy.com/v1/gifs';
 
   private _historial: string[] = [];
 
@@ -44,7 +46,12 @@ export class GifsService {
       
     }
 
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=b3SH9Mch5rxj2Jtgzwnpn31OLw8YavrD&q=${query}&limit=10`)
+    const params = new HttpParams().
+    set('api_key', this.apiKey)
+    .set('limit','10')
+    .set('q', query);
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`,{params})
     .subscribe( (resp) =>{
       console.log(resp.data)
       this.resultados = resp.data;
